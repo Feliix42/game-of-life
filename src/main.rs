fab273195dc12ee0162b6f7b060b5efc88be1565
Felix Wittwer <dev@felixwittwer.de>
2020-08-03 20:15:06 +0200

    Implement updating and drawing the universe

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+use ggez::event;
+use ggez::{conf, ContextBuilder};
+
 mod universe;
 
+use universe::Universe;
+
+static DIM_X: f32 = 800.0;
+static DIM_Y: f32 = 600.0;
+static SIZE_X: usize = 80;
+static SIZE_Y: usize = 60;
+
 fn main() {
-    println!("Hello World!");
+    let (mut context, mut event_loop) = ContextBuilder::new("Game of Life", "Felix Wittwer")
+        .window_mode(conf::WindowMode::default().dimensions(DIM_X, DIM_Y))
+        .window_setup(conf::WindowSetup::default().title("Conway's Game of Life"))
+        .build()
+        .unwrap();
+
+    let mut universe = Universe::initialize((SIZE_X, SIZE_Y), None);
+
+    event::run(&mut context, &mut event_loop, &mut universe).unwrap();
 }
--- src/universe.rs
+++ src/universe.rs
@@ -1,12 +1,17 @@
-// use rand;
-use rand::{RngCore, SeedableRng};
+use ggez::event::EventHandler;
+use ggez::graphics;
+use ggez::graphics::spritebatch::SpriteBatch;
+use ggez::graphics::{DrawParam, Image};
+use ggez::nalgebra::Point2;
+use ggez::{Context, GameResult};
 use rand::rngs::SmallRng;
+use rand::{RngCore, SeedableRng};
 
 /// State of an individual cell
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq)]
 pub enum CellState {
     Live,
-    Dead
+    Dead,
 }
 
 /// The universe the game is set in
@@ -39,12 +44,92 @@ impl Universe {
 
         Self {
             dimensions,
-            neighborhood
+            neighborhood,
         }
     }
 
     /// Compute the next generation of the universe.
     pub fn advance_generation(&mut self) {
-        unimplemented!()
+        // clone the neighborhood to be able to just keep the non-changed data
+        let mut new_neighborhood = self.neighborhood.clone();
+
+        let (x_max, y_max) = self.dimensions;
+        for x in 0..x_max {
+            for y in 0..y_max {
+                // check all directions, count # of neighbors
+                let mut life_neighbors: u8 = 0;
+                if x > 0 {
+                    if y > 0 && self.neighborhood[x - 1][y - 1] == CellState::Live {
+                        life_neighbors += 1;
+                    }
+                    if self.neighborhood[x - 1][y] == CellState::Live {
+                        life_neighbors += 1;
+                    }
+                    if y < (y_max - 1) && self.neighborhood[x - 1][y + 1] == CellState::Live {
+                        life_neighbors += 1;
+                    }
+                }
+
+                if y > 0 && self.neighborhood[x][y - 1] == CellState::Live {
+                    life_neighbors += 1;
+                }
+                if y < (y_max - 1) && self.neighborhood[x][y + 1] == CellState::Live {
+                    life_neighbors += 1;
+                }
+
+                if x < (x_max - 1) {
+                    if y > 0 && self.neighborhood[x + 1][y - 1] == CellState::Live {
+                        life_neighbors += 1;
+                    }
+                    if self.neighborhood[x + 1][y] == CellState::Live {
+                        life_neighbors += 1;
+                    }
+                    if y < (y_max - 1) && self.neighborhood[x + 1][y + 1] == CellState::Live {
+                        life_neighbors += 1;
+                    }
+                }
+
+                if self.neighborhood[x][y] == CellState::Live {
+                    if life_neighbors != 2 && life_neighbors != 3 {
+                        new_neighborhood[x][y] = CellState::Dead;
+                    }
+                } else {
+                    if life_neighbors == 3 {
+                        new_neighborhood[x][y] = CellState::Live;
+                    }
+                }
+            }
+        }
+
+        self.neighborhood = new_neighborhood;
+    }
+}
+
+impl EventHandler for Universe {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        self.advance_generation();
+
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, graphics::WHITE);
+
+        let box_shape_thingy = Image::solid(ctx, 10, graphics::BLACK).unwrap();
+        let mut batch = SpriteBatch::new(box_shape_thingy);
+
+        let (x_max, y_max) = self.dimensions;
+        for x in 0..x_max {
+            let x_coord = x as f32 * 10.0;
+            for y in 0..y_max {
+                if self.neighborhood[x][y] == CellState::Live {
+                    batch.add(DrawParam::new().dest(Point2::new(x_coord, y as f32 * 10.0)));
+                }
+            }
+        }
+
+        graphics::draw(ctx, &batch, DrawParam::default()).unwrap();
+
+        graphics::present(ctx)
     }
 }
